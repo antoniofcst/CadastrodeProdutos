43ac3efe044650c7f7878035ddf45f5809a22ad0
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-12-12 23:14:07 -0300

    Implementando os métodos de delete, salvar e alterar

--- app/src/main/java/com/example/cadastrodeprodutos/BDHelper/ProdutosBd.java
+++ app/src/main/java/com/example/cadastrodeprodutos/BDHelper/ProdutosBd.java
@@ -42,6 +42,22 @@ public class ProdutosBd extends SQLiteOpenHelper {
         getWritableDatabase().insert("produtos", null, values);
     }
 
+    public void alterarProduto(Produtos produto) {
+        ContentValues values = new ContentValues();
+
+        values.put("nomeproduto", produto.getNomeProduto());
+        values.put("descricao", produto.getDescricaoProduto());
+        values.put("quantidade", produto.getQuantidade());
+
+        String [] args = {produto.getId().toString()};
+        getWritableDatabase().update("produtos", values, "id=?", args);
+    }
+
+    public void deletarProduto(Produtos produto) {
+        String [] args = {produto.getId().toString()};
+        getWritableDatabase().delete("produtos", "id=?", args);
+    }
+
     public ArrayList<Produtos> getLista() {
         String [] columns = {"id", "nomeproduto", "descricao", "quantidade"};
         Cursor cursor = getWritableDatabase().query("produtos", columns, null, null, null, null, null , null);
--- app/src/main/java/com/example/cadastrodeprodutos/FormularioProdutos.java
+++ app/src/main/java/com/example/cadastrodeprodutos/FormularioProdutos.java
@@ -4,22 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.cadastrodeprodutos.BDHelper.ProdutosBd;
 import com.example.cadastrodeprodutos.model.Produtos;
 
 public class FormularioProdutos extends AppCompatActivity {
 
     EditText editTextNomeProduto, editTextdescricaoProduto, editTextquantidade;
     Button btn_poliform;
-    Produtos editarProduto;
+    Produtos editarProduto, produto;
+    ProdutosBd bdHelper;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_produtos);
+
+        bdHelper = new ProdutosBd(FormularioProdutos.this);
+
 
         Intent intent = getIntent();
         editarProduto = (Produtos) intent.getSerializableExtra("produto-escolhido");
@@ -36,6 +42,26 @@ public class FormularioProdutos extends AppCompatActivity {
             btn_poliform.setText("Cadastrar");
         }
 
+        btn_poliform.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                produto.setNomeProduto(editTextNomeProduto.getText().toString());
+                produto.setDescricaoProduto(editTextdescricaoProduto.getText().toString());
+                produto.setQuantidade(Integer.parseInt(editTextquantidade.getText().toString()));
+
+                if(btn_poliform.getText().toString().equals("Cadastrar")) {
+                    bdHelper.salvarProduto(produto);
+                    bdHelper.close();
+                } else {
+                    bdHelper.alterarProduto(produto);
+                    bdHelper.close();
+                }
+
+
+            }
+        });
 
     }
+
+
 }
